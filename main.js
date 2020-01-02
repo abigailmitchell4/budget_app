6637f6564e13014b4e3e4ff3fe74153acb469f72
Abigail Mitchell <abby.w.mitchell@gmail.com>
2020-01-01 22:06:59 -0700

    background-overlay

--- main.js
+++ main.js
@@ -1,6 +1,56 @@
 //Budget Controller
 const budgetController = (() => {
 
+  class Expense {
+    constructor(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+  }
+
+  class Income {
+    constructor(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+  }
+
+  let data = {
+      allItems: {
+        exp: [],
+        inc: []
+      },
+      totals: {
+        exp: 0,
+        inc: 0
+      }
+  }
+
+  //return public user input that creates a new object that is either an Expense or Income, depending on user input 
+  return {
+    addItem: (type, des, val) => {
+      let newItem;
+
+      // Create new ID for newItem
+      if (data.allItems[type].length > 0) {
+      ID = data.allItems[type][data.allItems[type].length-1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+      // push newItem to data
+      data.allItems[type].push(newItem);
+      return newItem;
+    }
+  };
+
 })();
 
 
@@ -12,6 +62,8 @@ const uiController = (() => {
     inputDescription: '.add__description',
     inputValue: '.add__value',
     inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   };
 
   return {
@@ -22,6 +74,25 @@ const uiController = (() => {
         value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       };
     },
+    addListItem: (obj, type) => {
+      let html, newHtml, element;
+
+      if (type === 'inc') {
+         element = DOMstrings.incomeContainer;
+
+         html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"> <i class="ion-ios-close-outline"></i> </button> </div> </div> </div>';
+      } else if (type === 'exp') {
+        element = DOMstrings.expensesContainer;
+
+        html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+      }
+
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
     getDOMstrings: () => {
       return DOMstrings;
     }
@@ -31,23 +102,38 @@ const uiController = (() => {
 
 
 //Global App Controller
-const controller = ((bugetCtrl, uiCtrl) => {
+const controller = ((budgetCtrl, uiCtrl) => {
 
-  let DOM = uiCtrl.getDOMstrings();
+  const setUpEventListeners = () => {
+    let DOM = uiCtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", (event) => {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
 
   const ctrlAddItem = () => {
+    let input, newItem; 
 
-    let input = uiCtrl.getinput();
-    console.log(input);
+    input = uiCtrl.getinput();
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+    uiCtrl.addListItem(newItem, input.type);
+  };
+
+  return {
+    init: () => {
+      console.log("App has started");
+      setUpEventListeners();
+    }
   }
 
-  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
-  document.addEventListener("keypress", (event) => {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
-    }
-  });
+})(budgetController, uiController);
 
-  })(budgetController, uiController);
+controller.init();
 
